344ad60ad5a2fdf045d50b9ed0253dd09e4133da
sieng86 <149126826+sieng86@users.noreply.github.com>
2026-03-24 12:04:10 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,33 +1,13 @@
-equire("express");
+const express = require("express");
+
 const app = express();
 
-app.use(express.json());
-
-let posts = [];
-
-// test
 app.get("/", (req, res) => {
-  res.send("API running 🚀");
+  res.send("OK RUNNING");
 });
 
-// get posts
-app.get("/posts", (req, res) => {
-  res.json(posts);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running on " + PORT);
 });
-
-// create post
-app.post("/posts", (req, res) => {
-  const { username, content } = req.body;
-
-  const newPost = {
-    id: Date.now(),
-    username: username || "Guest",
-    content
-  };
-
-  posts.unshift(newPost);
-  res.json(newPost);
-});
-
-const PORT = process.env.PORT || 10000;
-app.listen(PORT, () => console.log("Server running"));
